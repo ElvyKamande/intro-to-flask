b11ddf31048957f49cd91b215658c3ddeff08355
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-06-13 11:44:05 +0300

    create posts table

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_migrate import Migrate
+from models import db, User
+
+app =  Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+@app.route('/about')
+def about():
+    return "This is the about page."
+@app.route("/<username>")
+def username(username):
+    return f"Hello, {username}!"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500, host='0.0.0.0')
---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, primary_key=True)
+    last_name = db.Column(db.String, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
