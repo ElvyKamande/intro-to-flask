19f61c234eabb70bf838e5df81b52275afc62714
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-06-16 10:04:13 +0300

    return post dictionary

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_migrate import Migrate
 from models import db, User
+from flask_restful import Api, Resource
 
 app =  Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
 
 migrate = Migrate(app, db)
 db.init_app(app)
+
+api = Api(app)
 
 @app.route('/')
 def index():
@@ -24,3 +27,11 @@ def username(username):
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500, host='0.0.0.0')
+
+    
+    class PostEndpoint(Resource):
+        def get(self):
+            return {"message": "GET request received"}
+
+        def post(self):
+            return {"message": "POST request received"} 
--- models.py
+++ models.py
@@ -5,14 +5,14 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String, primary_key=True)
-    last_name = db.Column(db.String, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String, nullable=False)
+    heading = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(2000), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
